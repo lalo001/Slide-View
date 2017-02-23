8753b870ee3a1c9b4eb2d4cef1a4479694c50806
Eduardo Valencia <l.alo.red@hotmail.com>
2017-02-23 14:01:12 -0600

    Initial Commit

---
+++ Slide View/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Slide View
+//
+//  Created by Eduardo Valencia on 2/23/17.
+//  Copyright © 2017 Eduardo Valencia. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
